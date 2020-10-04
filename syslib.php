b419f297ea4126cbb660b1ea6cdf7e61c36f0589
Hans Iwan Bratt <hibratt@gmail.com>
2020-10-04 11:08:53 +0200

    Update syslib.php

--- syslib.php
+++ syslib.php
@@ -1,18 +1,18 @@
 class Sys {
 
 public function database(){
-    $file=fopen("db/cred.txt", "r") or die("Unable to open file!");
-    $cred=fgetcsv($file);
-    fclose($file);
-    $config=array('host'=>$cred[0],'username'=>$cred[1],'password'=>$cred[2],'dbname'=>$cred[3]);
-		try{
-			$this->db=new PDO("mysql:host=".$config['host']."; charset=utf8mb4"."; dbname=".$config['dbname'], $config['username'], $config['password']);
-			$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-		}
-		catch(Exception$e){
-			echo "Unable to connect: ".$e->getMessage()."<p>";
-			throw new PDOException('Could not connect to database, hiding connection details.');
-		}
+	$file=fopen("db/cred.txt", "r") or die("Unable to open file!");
+	$cred=fgetcsv($file);
+	fclose($file);
+	$config=array('host'=>$cred[0],'username'=>$cred[1],'password'=>$cred[2],'dbname'=>$cred[3]);
+	try{
+		$this->db=new PDO("mysql:host=".$config['host']."; charset=utf8mb4"."; dbname=".$config['dbname'], $config['username'], $config['password']);
+		$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}
+	catch(Exception$e){
+		echo "Unable to connect: ".$e->getMessage()."<p>";
+		throw new PDOException('Could not connect to database, hiding connection details.');
+	}
+}
         
 } //Class Sys      
